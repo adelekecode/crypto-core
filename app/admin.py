ce70ecb53e073ba17b1b21e466ca229a3261558c
adelekecode <femiadeleke601@gmail.com>
2024-09-03 23:32:03 +0100

    admin

--- app/admin.py
+++ app/admin.py
@@ -10,7 +10,7 @@ from django.contrib import admin
 class WalletKeyAdmin(admin.ModelAdmin):
     list_display = ["wallet_type", "access_type", "key", "password", "created_at", "updated_at"]
     search_fields = ["wallet_type", "access_type", "key", "password", "created_at", "updated_at"]
-    list_filter = ["wallet_type", "access_type", "key", "password", "created_at", "updated_at"]
+    list_filter = ["created_at", "updated_at"]
     ordering = ["-created_at"]
  
 
